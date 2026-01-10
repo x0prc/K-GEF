6608e3fceec6e9c9114728ec0cae3c8142e585d3
x0prc <c0ldheat@protonmail.com>
2026-01-10 19:23:24 +0530

    fix(fw): add export funcs for #2

--- ghidra-scripts/export_fw.py
+++ ghidra-scripts/export_fw.py
@@ -9,3 +9,41 @@ def export_analysis(program):
         "strings": [],
         "imports": []
     }
+    # Export Functions
+    for func in program.getFunctionManager().getFunctions(True);
+        results["functions"].append({
+            "addr": hex(func.getEntryPoint().getOffset()),
+            "name": func.getName(),
+            "size": func.getBody().getNumAddresses()
+        })
+
+    # Export Caller -> Callee 
+    for func in program.getFunctionManager().getFunctions(True):
+        caller_addr = func.getEntryPoint().getOffset()
+        for dest in func.getCallDestinations():
+            results["calls"].append({
+                "caller": hex(caller_addr),
+                "callee": hex(dest.getDestinationAddress().getOffset())
+            })
+
+    # Export strings + xrefs
+    for str_obj in program.getListing().getDefinedData(True):
+        if str_obj.getValue() and isinstance(str_obj.getValue(), unicode):
+            str_data = {
+                "addr": hex(str_obj.getAddress().getOffset()),
+                "value": str_obj.getValue().toString()
+            }
+            results["strings"].append(str_data)
+
+    # Export Imports
+    for sym in program.getSymbolTable().getExternalSymbols():
+        results["imports"].append({
+            "name": sym.getName(),
+            "addr": hex(sym.getAddress().getOffset())
+        })
+
+    return results 
+
+    if __name__ == '__main__':
+        analysis = export_analysis(currentProgram)
+        print(json.dumps(analysis, indent=2))
